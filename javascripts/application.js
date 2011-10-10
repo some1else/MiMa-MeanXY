a65c3f43316dccbb2a186b808d96d7ac6ad3e595
Srdjan Prodanovic <srdjan.prodanovic@gmail.com>
2011-10-10 19:58:36 +0200

    Backbone Model, Collection and View classes

--- javascripts/application.js
+++ javascripts/application.js
@@ -3,24 +3,48 @@ var data = {"tiles":["+mlakar","#awesome","#now","@cool","#mima","+vida","#proje
 //TODO breadcrumb or query display
 //TODO blank slate creators
 
-var Tile = Backbone.Model.extend({
+var TileModel = Backbone.Model.extend({
   initialize: function(obj) {
-    this.set({content: obj.content.substr(1, obj.content.length)})
+    var kind = obj.text.substr(0, 1)
+    var content = obj.text.substr(1, obj.text.length)
+    switch (kind) {
+      case "+": kind = "person"
+                break
+      case "#": kind = "tag"
+                break
+      case "@": kind = "place"
+                break
+      case "~": kind = "time"
+                break
+      case "?": kind = "query"
+    }
+    this.set({content: content, kind: kind})
   }
 })
 
-var Person = Tile.extend({
-
+var TileCollection = Backbone.Collection.extend({
+  model: "TileModel"
 })
 
-var Place = Tile.extend({
-  
+var TileView = Backbone.View.extend({
+  tagName: "li", className: "tile",
+  events: {
+    "click a": "query" 
+  },
+  render: function() {
+    console.log("x")
+  }
 })
 
-var Tag = Tile.extend({
-  
+_.each(data.tiles, function(tile) {
+  var t = new TileModel({text: tile})
+  console.log(t.get("content"))
 })
 
-var Time = Tile.extend({
-  
-})
+
+
+var firstRequest = function() {
+  $.ajax("/first")
+}
+
+//Tiles.reset(firstRequest())
